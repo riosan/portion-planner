f767c43ca91fcdcb39f8e7d553bbe26e2d07f792
Alex <yaylov@gmail.com>
2026-09-05 00:03:44 +0200

    fix error camer4

--- app.js
+++ app.js
@@ -776,7 +776,7 @@ function openOcrModalAndGetName() {
           alert("No text detected in the frame.");
         }
       } catch (error) {
-        console.error("Scan error:", error);
+        //console.error("Scan error:", error);
         // alert("Scanning error: " + (error.message || error));
       } finally {
         ocrBtn.disabled = false;
